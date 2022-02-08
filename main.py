1ad68d80c5002cf38a16eae3556f26ecb0b05ff8
BryanNsoh <bryan.anye.5@gmail.com>
2022-02-07 19:18:39 -0800

    Intermediate Changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import serial
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget,\
     QVBoxLayout, QHBoxLayout, QStackedLayout, QPushButton, QLabel,\
-    QFrame, QGridLayout, QLineEdit, QRadioButton
+    QFrame, QGridLayout, QLineEdit, QRadioButton, QGroupBox
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtCore import Qt, QSize
 import matplotlib.pyplot as plt
@@ -90,6 +90,51 @@ class MainWindow(QMainWindow):
         stop_button = QPushButton("Stop")
         motion_control_layout.addWidget(stop_button)
         stop_button.clicked.connect(stop_button_clicked)
+        
+        
+        # scanning status
+        scanning_status_layout = QGridLayout()
+        scanning_status_title = QLabel("Scanning Status")
+        scanning_status_title.setAlignment(Qt.AlignCenter)
+        scanning_status_title.setStyleSheet("font: bold 30px")
+
+        scanning_status_layout.addWidget(scanning_status_title, 0, 1, 1, 3)  # adding widget to frame .. grid layout
+
+        # Enable and Disable buttons
+
+        # Group these buttons together ```````````````````````````  AUDIT
+        groupbox = QGroupBox("Scan Function")
+        layout = QGridLayout()
+        self.setLayout(layout)
+        vbox = QVBoxLayout()
+        groupbox.setLayout(vbox)
+
+        self.enable = QRadioButton("Scan Function Enable")
+        self.enable.setLayoutDirection(Qt.RightToLeft)
+        vbox.addWidget(self.enable)     # Adding this radiobutton to the group  ```````````AUDIT
+        scanning_status_layout.addWidget(self.enable, 1, 2)
+        self.disable = QRadioButton("Scan Function Disable")
+        self.disable.setLayoutDirection(Qt.RightToLeft)
+        scanning_status_layout.addWidget(self.disable, 2, 2)
+        vbox.addWidget(self.disable)    # Adding this radiobutton to the group
+
+        # Forward or backward scan
+        self.forward = QRadioButton("Scan on Forward")
+        self.forward.setLayoutDirection(Qt.RightToLeft)
+        scanning_status_layout.addWidget(self.forward, 3, 2)
+        self.backward = QRadioButton("Scan on Backward")
+        self.backward.setLayoutDirection(Qt.RightToLeft)
+        scanning_status_layout.addWidget(self.backward, 4, 2)
+
+        # Scan Rate
+        scan_rate = QLabel("Encoder pulses per scan\n(Scan rate)")
+        scanning_status_layout.addWidget(scan_rate, 7, 0)
+        begin_input = QLineEdit()  # create input field
+        scan_rate.setLayoutDirection(Qt.RightToLeft)
+        begin_input.setLayoutDirection(Qt.RightToLeft)  #
+        scanning_status_layout.addWidget(begin_input, 7, 2, 1, 1)
+
+
 
         #scan control
         scan_control_layout = QGridLayout()
@@ -124,10 +169,11 @@ class MainWindow(QMainWindow):
 
 
 
-        #main layout
+        #main layout 
         mainlayout.addLayout(motion_control_layout,0,0)
         mainlayout.addLayout(scan_control_layout,1,0)
         mainlayout.addLayout(output_data_layout, 0, 1, 2, 4)
+        mainlayout.addLayout(scanning_status_layout, 0, 2)      # Layout of scanning status frame
 
         widget = QWidget()
         widget.setLayout(mainlayout)
